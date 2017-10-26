f4f071ec6c8a25442207d403ec5b73e329a46e7d
Andre Chan <andre_h_chan@hotmail.com>
2017-10-26 10:01:01 -0700

    more codacy changes

--- app/src/main/java/com/example/andre/qfinder/DisplayMessageActivity.java
+++ app/src/main/java/com/example/andre/qfinder/DisplayMessageActivity.java
@@ -3,7 +3,6 @@ package com.example.andre.qfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
