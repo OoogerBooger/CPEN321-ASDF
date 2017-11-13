aba77d5e14e1130ba49bf7f07c6a580389637d83
Andre Chan <andre_h_chan@hotmail.com>
2017-11-13 11:37:27 -0800

    Room Joining Debugged

---
+++ app/src/main/java/com/example/andre/qfinder/InvalidRoomCode.java
@@ -0,0 +1,13 @@
+package com.example.andre.qfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvalidRoomCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invalid_room_code);
+    }
+}
